1fa1c8ccd835a48e11be30021957eb0a53669602
Kavya Singampalli <kavyasingampalli@Kavyas-MacBook-Air.local>
2025-03-22 12:18:44 +0530

    homeScreen

--- App.js
+++ App.js
@@ -1,44 +1,18 @@
 
 import { SafeAreaView, StyleSheet,View } from 'react-native';
+import LaunchScreen from './components/launchsignInScreen/Launchscreen';
 
-import Mainscreen from './components/admin/Mainscreen';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
 
-import NewPatient from './components/admin/NewPatient';
-import Appointments from './components/admin/Appointments';
-
-import BookAppointment from './components/admin/BookAppointment';
-import DoctorList from './components/admin/DoctorList';
-import PatientDetails from './components/admin/PatientDetails';
-import ActionSheetMore from './components/admin/ActionSheetMore';
-
-const Stack=createStackNavigator();
 export default function App() {
   return (
-   <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown:false}}>
-          <Stack.Screen name="Mainscreen" component={Mainscreen} />
-          <Stack.Screen name="NewPatient" component={NewPatient} />
-          <Stack.Screen name="Appointments" component={Appointments}/>
-          <Stack.Screen name='DoctorList' component={DoctorList}/>
-          <Stack.Screen name='BookAppointment' component={BookAppointment}/>
-          <Stack.Screen name='PatientDetails' component={PatientDetails}/>
-          <Stack.Screen name='ActionSheetMore' component={ActionSheetMore}/>
-          
-        </Stack.Navigator>
-      </NavigationContainer>
-      
-    </SafeAreaView>
-
-   
+   <View style={styles.container}>
+     <LaunchScreen/>  
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F0FAF3',
   },
 });
---
+++ components/launchsignInScreen/Launchscreen.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { StyleSheet, View ,Text, TouchableOpacity} from "react-native";
+
+
+
+export default function LaunchScreen(){
+    return(
+        <View style={styles.container}>
+            
+             <Text style={styles.text}>CureSync</Text>
+             <Text style={styles.subText}>Join us for seamless Health Management and Support</Text>
+             <TouchableOpacity style={styles.buttonLogIn}>
+                <Text style={styles.logTxt}>LogIn</Text>
+             </TouchableOpacity>
+             <TouchableOpacity style={styles.buttonSign}>
+                <Text style={styles.signTxt}>Sign Up</Text>
+             </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+       flex:1,
+       backgroundColor:"#6A81D5",
+    },
+   text:{
+    position: "absolute",
+    top: 404, 
+    left: 100,
+    fontSize:40,
+    fontWeight:"700",
+    lineHeight:56,
+    color:"#FFFFFF",
+   },
+   subText:{
+    position:"absolute",
+    top:472,
+    left:45,
+    color:"#FFFFFF",
+    width:300,
+    fontSize:16,
+    fontWeight:"400",
+    lineHeight:26,
+    textAlign:"center"
+   },
+   buttonLogIn:{
+    position:"absolute",
+    top:688,
+    left:20,
+    width:350,
+    height:52,
+    backgroundColor:"#F3F5FCFF",
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"center",
+    borderRadius:3,
+   },
+   logTxt:{
+    fontSize:18,
+    fontWeight:"400",
+    color:"#6A81D5"
+   },
+   buttonSign:{
+    position:"absolute",
+    top:756,
+    left:20,
+    width:350,
+    height:52,
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"center",
+    borderWidth:2,
+    borderColor:"white",
+    borderRadius:3
+   },
+   signTxt:{
+    fontSize:18,
+    fontWeight:"400",
+    color:"white"
+   }
+})
