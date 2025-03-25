cba4cf30f7df2a1142d8041bc7442682356dcde4
Srikanth Bathi <srikanth.bathi@yahooinc.com>
2025-03-25 08:59:29 +0530

    home screen

--- components/admin/HomeScreen.js
+++ components/admin/HomeScreen.js
@@ -57,11 +57,19 @@ export default function HomeScreen() {
                     </View>
                 </View>
                 <View style={{ marginVertical: 10 }} />
-                <View >
-                    <Text style={styles.headerText}>Current Appointments</Text>
-                </View>
-                <AppointmentList />
-               
+                <View style={styles.contentContainer}>
+                        <View style={styles.leftContainer}>
+                            <Text style={styles.leftHead}>Track Patients</Text>
+                            <Text style={styles.leftDescription}>Track all the available patients..</Text>
+                            <TouchableOpacity style={styles.leftButton} onPress={() => navigation.navigate("NewPatient")}>
+                                <Text style={styles.leftButtonText}>Track Now</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <Image style={styles.png} source={require("../../assets/trackapp.png")} />
+                        </View>
+                    </View>
+                <View style={{ marginVertical: 10 }} />
             </View>
         </SafeAreaView>
     )
