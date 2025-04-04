f9627970956facb1a3f180c67e68461248e520c9
Srikanth Bathi <srikanth.bathi@yahooinc.com>
2025-04-04 23:29:48 +0530

    loading indicator overlay

--- src/components/MdLogActivityIndicator.tsx
+++ src/components/MdLogActivityIndicator.tsx
@@ -1,32 +1,41 @@
 import { COLORS } from '@utils/colors';
 import React, { useState } from 'react';
 import { View, ActivityIndicator, StyleSheet, Button } from 'react-native';
+import { Dimensions } from 'react-native';
+const { height, width } = Dimensions.get('window');
 
 export const MdLogActivityIndicator = ({loading}) => {
 
-  return (
-    <View style={styles.container}>
-      {loading && (
-        <View style={styles.loaderContainer}>
+  if(loading){
+    return(
+      <View style={styles.loaderContainer}>
           <ActivityIndicator size="large" color={COLORS.red} />
-        </View>
-      )}
-    </View>
-  );
+        </View> 
+    )
+  }else{
+    <></>
+  }
+
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    position: 'absolute',
+    top: 0, left: 0, right: 0, bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)', // dim the background
     justifyContent: 'center',
     alignItems: 'center',
-    zIndex:6000
+    zIndex: 9999 // ensure it's on top
   },
   loaderContainer: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'rgba(218, 23, 23, 0.5)', // Semi-transparent background
+    //backgroundColor: 'rgba(218, 23, 23, 0.5)', // Semi-transparent background
+    position: 'absolute',
+    top: 0, left: 0, right: 0, bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)', // dim the background
     justifyContent: 'center',
     alignItems: 'center',
-    zIndex: 1000, // Ensure it's above other UI elements
+    zIndex: 9999, // ensure it's on top
+    height: height
   },
 });
